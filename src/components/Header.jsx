501a2ab1357e1904032a923866c9f98cd1deb10d
Alexey Ponomarev <ponomarevmgn@gmail.com>
2022-06-29 08:16:51 +0500

    feat: add routing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import { Routes, Route, NavLink } from 'react-router-dom';
 import logo from '../image/Vector.svg';
 
 function Header({ textLink }) {
   return (
     <header className='header'>
       <img className='header__logo' src={logo} alt='Логотип'/>
-      <a className='header__link'>{textLink}</a>
+      <Routes>
+        <Route 
+          path='/'
+          element={<NavLink className='header__link' to='/sign-in'>Выйти</NavLink>}
+        />
+                <Route 
+          path='/sign-in'
+          element={<NavLink className='header__link' to='/sign-up'>Регистрация</NavLink>}
+        />
+                <Route 
+          path='/sign-up'
+          element={<NavLink className='header__link' to='/sign-in'>Войти</NavLink>}
+        />
+      </Routes>
     </header>
   )
 };
